c14af89cc9007ce4b568c04eefb267b98ea2e015
Anastasia <ana.ko8915@gmail.com>
2023-10-30 13:43:40 +0200

    page navigation

--- src/components/Main/PageNavigation.tsx
+++ src/components/Main/PageNavigation.tsx
@@ -1,10 +1,37 @@
+// PageNavigation.tsx
+
 import React from "react";
 import "./PageNavigation.scss";
 
 const PageNavigation: React.FC = () => {
   return (
     <div className="page-nav-wrapper col-12">
-      {/* ... content of page navigation ... */}
+      <div className="page-nav-box">
+        <a href="#">
+          <div className="prev-page">OLDER POST</div>
+        </a>
+        <a href="#">
+          <div className="page-num">1</div>
+        </a>
+        <a href="#">
+          <div className="page-num">2</div>
+        </a>
+        <a href="#">
+          <div className="page-num">3</div>
+        </a>
+        <a href="#">
+          <div className="page-num">...</div>
+        </a>
+        <a href="#">
+          <div className="page-num last">8</div>
+        </a>
+        <a href="#">
+          <div className="next-page">
+            NEXT POST
+            <div className="arrow-next"></div>
+          </div>
+        </a>
+      </div>
     </div>
   );
 };
