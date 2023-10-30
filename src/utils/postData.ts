04abecc80d49d0a542f8c668ae34e4472b238080
Anastasia <ana.ko8915@gmail.com>
2023-10-30 11:49:15 +0200

    rough main component

--- src/utils/postData.ts
+++ src/utils/postData.ts
@@ -5,6 +5,8 @@ export type Post = {
   title: string;
   author: string;
   date: string;
+  featured?: boolean;
+  content?: string;
 };
 
 export const posts: Post[] = [
@@ -52,6 +54,49 @@ export const posts: Post[] = [
     id: 6,
     imageUrl: "images/post-image6.png",
     category: "TOURISM",
+    title: "One of Saturn’s largest rings may be newer than anyone",
+    author: "By Rickie Baroch",
+    date: "October 26, 2023",
+  },
+  {
+    id: 7,
+    imageUrl: "images/feat-post-main.png",
+    category: "TOURISM",
+    title: "One of Saturn’s largest rings may be newer than anyone",
+    author: "By Rickie Baroch",
+    date: "October 26, 2023",
+    featured: true,
+    content:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem",
+  },
+  {
+    id: 8,
+    imageUrl: "images/post-image3.png",
+    category: "TOURISM",
+    title: "One of Saturn’s largest rings may be newer than anyone",
+    author: "By Rickie Baroch",
+    date: "October 26, 2023",
+  },
+  {
+    id: 9,
+    imageUrl: "images/post-image4.png",
+    category: "SPORT",
+    title: "One of Saturn’s largest rings may be newer than anyone",
+    author: "By Rickie Baroch",
+    date: "October 26, 2023",
+  },
+  {
+    id: 10,
+    imageUrl: "images/post-image5.png",
+    category: "FASHION",
+    title: "One of Saturn’s largest rings may be newer than anyone",
+    author: "By Rickie Baroch",
+    date: "October 26, 2023",
+  },
+  {
+    id: 11,
+    imageUrl: "images/post-image6.png",
+    category: "FASHION",
     title: "One of Saturn’s largest rings may be newer than anyone",
     author: "By Rickie Baroch",
     date: "October 26, 2023",
