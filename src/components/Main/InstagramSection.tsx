eef4e7bf8e20ad56c99dd4229ff6bcb4530ad2a6
Anastasia <ana.ko8915@gmail.com>
2023-10-30 13:16:44 +0200

    carousel fix

--- src/components/Main/InstagramSection.tsx
+++ src/components/Main/InstagramSection.tsx
@@ -1,47 +1,53 @@
-import React, { useState, useEffect } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import "./InstagramSection.scss";
 
 const InstagramSection = () => {
-  const [isCarouselVisible, setCarouselVisible] = useState(false);
-
-  useEffect(() => {
-    setCarouselVisible(true);
-  }, []);
-
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    swipe: true,
-  };
-
   const images = Array(6)
     .fill(0)
     .map((_, index) => `/images/photo${index + 1}.png`);
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 4,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 4,
+    },
+  };
 
   return (
     <div className="instagram-section">
       <div className="insta-main">
         Follow our <a href="#">@instagram_name</a>
       </div>
-      {isCarouselVisible && (
-        <Slider {...settings} className="insta-photos">
-          {images.map((image, index) => (
-            <div key={index} className="instagram-image">
-              <a href="#">
-                <img src={image} alt={`Instagram Image ${index + 1}`} />
-              </a>
-            </div>
-          ))}
-        </Slider>
-      )}
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        swipeable={true}
+        draggable={true}
+        showDots={false}
+        arrows={true}
+        centerMode={true}
+        focusOnSelect={true}
+      >
+        {images.map((image, index) => (
+          <div key={index} className="instagram-image">
+            <img src={image} alt={`Instagram Image ${index + 1}`} />
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 };
