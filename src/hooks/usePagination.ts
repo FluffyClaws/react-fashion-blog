9289f82668327c131f33b39b30808c24f13f85f3
Anastasia <ana.ko8915@gmail.com>
2023-11-04 21:13:24 +0200

    typo fix

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -13,32 +13,25 @@ const usePagination = <
   const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
-    // Sort the data first
-    const sortedData = sortPostsByDate<T>(data); // Ensure sortPostsByDate accepts generic type T
+    const sortedData = sortPostsByDate<T>(data);
 
-    // Then filter the sorted data
     const filteredData = sortedData.filter(filterCondition);
 
-    // Find the featured post only in the filtered data
     const featuredPost =
       currentPage === 1 ? filteredData.find((item) => item.featured) : null;
 
-    // Calculate the starting index of the items on the current page
     const indexOfFirstItem = (currentPage - 1) * itemsPerPage;
 
-    // Calculate the total number of pages based on the filtered data
     const newTotalPages = Math.ceil(
       (filteredData.length - (featuredPost ? 1 : 0)) / itemsPerPage
     );
 
-    // Adjust the paginated data based on the filtered data
     let newPaginatedData = filteredData
       .filter((item) => !item.featured)
       .slice(indexOfFirstItem, indexOfFirstItem + itemsPerPage);
 
-    // If there's a featured post on the first page, insert it at the specified index
     if (featuredPost && currentPage === 1) {
-      newPaginatedData.splice(4, 0, featuredPost); // Ensure the index is correct for your use case
+      newPaginatedData.splice(4, 0, featuredPost);
     }
 
     setPaginatedData(newPaginatedData);
