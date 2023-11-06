cc48901125cfe76a5b1b73705247fe5d9782b340
Anastasia <ana.ko8915@gmail.com>
2023-11-06 11:33:49 +0200

    removed unused files added ghpages

--- src/components/Main/Posts.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PostList from "../PostList/PostList";
-import { PostListProps } from "../../types/types";
-
-const Posts: React.FC<PostListProps> = ({ posts }) => {
-  return <PostList posts={posts} />;
-};
-
-export default Posts;
